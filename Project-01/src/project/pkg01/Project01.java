05834f3d17e4fdcde6dbd8f8d6c64f65729fa46f
Allan cardona <3004995740101@ingenieria.usac.edu.gt>
2024-07-11 12:58:57 -0600

    Hola Mundo

---
+++ Project-01/src/project/pkg01/Project01.java
@@ -0,0 +1,11 @@
+
+package project.pkg01;
+
+public class Project01 {
+
+    public static void main(String[] args) {
+        //Atajo para escribir más rapido el System.out.prinln() => escribir sout y precionar la tecla TAB
+        System.out.println("Hola estoy aprendiendo a programas con Java");
+    }
+    
+}
